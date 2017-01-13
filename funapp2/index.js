88868186efe3cd68e3b69d8dd5d49801475a6460
Gaurav Mamidwar <gaurav.mamidwar@idyllic-software.com>
2017-01-13 18:55:04 +0530

    GEM : get random number
    from module

--- funapp2/index.js
+++ funapp2/index.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var random_number = require('./random_number');
 
 requestHandler = function(req, res) {
   console.log('inside requestHandler');
+  console.log(process.argv[0]);
   if(req.url == '/'){
-    res.end('welcome home !');
+    var results = [];
+    for (var count =0; count < 3; count++) {
+      results.push(random_number());
+    }
+    res.end(results.join(','));
   } else {
     res.end('route not found.')
   }

26654a360c9965df3a768815d7671a71fd8f9c22
Gaurav Mamidwar <gaurav.mamidwar@idyllic-software.com>
2017-02-03 18:43:58 +0530

    added gitignore

--- app3feb/block_ip.js
+++ app3feb/block_ip.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var morgan = require('morgan');
+
+app.use(morgan('default'));
 
 var pub_path = path.resolve(__dirname, 'public');
 app.use(express.static(pub_path));

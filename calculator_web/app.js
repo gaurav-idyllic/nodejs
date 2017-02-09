f04120cdc555492142d5a8694bdc2c07d8dfc6fd
Gaurav Mamidwar <gaurav.mamidwar@idyllic-software.com>
2017-02-10 01:01:28 +0530

    calculator web assignment

---
+++ calculator_web/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var morgan = require('morgan');
+var mustache = require('mustache');
+var calculator = require('./calculator');
+
+app.set("view engine", "ejs");
+app.use(morgan('combined'));
+var pub_path = path.resolve(__dirname, 'public');
+app.use(express.static(pub_path));
+
+
+app.get('/', function(req, res, next) {
+  console.log('root path');
+  title = 'calculator web';
+  res.render("index");
+});
+
+app.get('/perform_operation', function(req, res, next){
+  input_params = req.query;
+  output = calculator(input_params);
+  res.send({ output: output});
+});
+
+app.use(function(req, res){
+  res.status(200).send('arerererer ');
+});
+
+
+
+app.listen(3002);
---
+++ calculator_web/public/main.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('.action_button').click(function(){
+    var param1 = $('.input1').val();
+    var param2 = $('.input2').val();
+    var action = $(this).data('action');
+    $.ajax({
+      type: 'GET',
+      data: {
+        param1: param1,
+        param2: param2,
+        action: action
+      },
+      url: '/perform_operation',
+      success: function(data){
+        $('.result_label').text(data.output);
+      },
+      fail: function(data){
+        alert('failed operation');
+      }
+    });
+  });
+});
